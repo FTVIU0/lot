b1cdf3395986722ce748b8e3a9b9f69578204a37
FTVIU0 <HGQ577068802@163.com>
2015-12-01 08:32:43 +0800

    测试串口发送数据

---
+++ lot_test/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lot_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            int Sdata = this.serialPort1.ReadByte();
+
+            this.textBox1.Invoke(
+                new MethodInvoker(
+                    delegate
+                    {
+                        this.textBox1.AppendText(Sdata.ToString());
+
+
+            
+                    }
+                    )
+                );
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Open();
+        }
+    }
+}
